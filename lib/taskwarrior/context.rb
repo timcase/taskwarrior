6d1b678acbaf7c764cf5f43e9c555683818bd42e
Tim Case <tim@2drops.net>
2018-04-03 14:41:32 +0700

    Add slug

--- lib/taskwarrior/context.rb
+++ lib/taskwarrior/context.rb
@@ -1,11 +1,12 @@
 module Taskwarrior
   class Context
-    attr_reader :name, :definition, :active
+    attr_reader :name, :definition, :active, :slug
 
     def initialize(row)
       @name = row[0]
       @definition = row[1]
       @active = row[2] == "yes" ? true : false
+      @slug = @name
     end
 
   end
