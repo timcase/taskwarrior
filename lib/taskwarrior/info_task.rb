f3739432080f7560d160fe04a43f793d7931e61a
Tim Case <tim@2drops.net>
2019-05-14 09:54:47 +0700

    Change methods to readers

--- lib/taskwarrior/info_task.rb
+++ lib/taskwarrior/info_task.rb
@@ -4,61 +4,26 @@ module Taskwarrior
   class InfoTask
     include Base::Report
 
+    attr_reader :uuid, :description, :project, :due, :wait, :start, :end_date,
+      :scheduled, :until, :priority, :recurrence, :status, :tags, :fields
+
     def initialize(rows)
       @rows = rows
-    end
-
-    def uuid
-      @uuid ||= extract_value("uuid")
-    end
-
-    def description
-      @description ||= extract_value("description")
-    end
-
-    def project
-      @project ||= extract_value("project")
-    end
-
-    def due
-      @due ||= extract_value("due")
-    end
-
-    def wait
-      @wait ||= extract_value("waiting_until")
-    end
-
-    def start
-      @start ||= extract_value("start")
-    end
-
-    def end_date
-      @end_date ||= extract_value("end")
-    end
-
-    def scheduled
-      @scheduled ||= extract_value("scheduled")
-    end
-
-    def until
-      @until ||= extract_value("until")
-    end
-
-    def priority
-      @priority ||= extract_value("priority")
-    end
-
-    def recurrence
-      @recurrency ||= extract_value("recurrence")
-    end
-
-    def status
-      @status ||= extract_value('status')
-    end
-
-    def tags
-      res = extract_value("tags")
-      @tags ||= res.nil? ? [] : res.split("\s")
+      @uuid = extract_value("uuid")
+      @description = extract_value("description")
+      @project = extract_value("project")
+      @due = extract_value("due")
+      @wait = extract_value("waiting_until")
+      @start = extract_value("start")
+      @end_date = extract_value("end")
+      @scheduled = extract_value("scheduled")
+      @until = extract_value("until")
+      @priority = extract_value("priority")
+      @recurrence = extract_value("recurrence")
+      @status = extract_value('status')
+      @tags = extract_value("tags")
+      @tags = @tags.nil? ? [] : @tags.split("\s")
+      @fields = data.to_h
     end
 
     def names
@@ -68,15 +33,10 @@ module Taskwarrior
     def values
       data_rows.map{|r| r.last.encode('UTF-8', invalid: :replace,
                                       undef: :replace).gsub('�', '').strip }
-
     end
 
     def data
       names.zip(values)
-    end
-
-    def fields
-      @fields ||= data.to_h
     end
 
     private
@@ -106,7 +66,6 @@ module Taskwarrior
       urgency_row = split_rows.detect{|r| r.first == "Urgency"}
       split_rows[0..split_rows.index(urgency_row)-1]
     end
-
 
     def method_names
       names.map{|n| n.downcase.gsub(" ", "_")}
